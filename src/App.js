55d399692b5d3e11ea766c7cf555b6e0d4f6ce0b
Godreigneth <godreignethlevidibia@yahoo.com>
2021-08-04 21:47:50 +0000

    Updated Name

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
+        <h1 className="f1">MyRoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filterRobots} />,
       </div>
